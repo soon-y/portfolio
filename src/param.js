df49f660013324cddc7d60f8576e991ebae3fba8
Soonyoung Park <soon.syp@gmail.com>
2025-03-19 21:32:29 +0100

    change matcap sky

--- src/param.js
+++ src/param.js
@@ -28,7 +28,7 @@ const param = {
   matcapRose: 'A48DA4_E8DDE8_C9B7C9_D4C2D4',
   matcapWhite: '9F9F9F_E4E4E4_D4D4D4_CCCCCC',
   matcapOrange: '9B4816_E8A138_CC7421_DC8827',
-  matcapSky: '65A0C7_C3E4F8_A7D5EF_97CAE9',
+  matcapSky: '346088_6ABED7_56A0C5_4E91B8',
   matcapBlue: '254FB0_99AFF0_6587D8_1D3279',
 };
 
